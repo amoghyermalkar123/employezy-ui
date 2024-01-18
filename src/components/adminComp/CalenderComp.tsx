82ff06c3018ef088bad6b20b752163522ce1ba06
platinumpizza29 <kbelgi1gmail.com>
2024-01-18 17:06:18 +0000

    Rendered UserId on calender

--- src/components/adminComp/CalenderComp.tsx
+++ src/components/adminComp/CalenderComp.tsx
@@ -1,94 +1,3 @@
-// import FullCalendar from "@fullcalendar/react";
-// import dayGridPlugin from "@fullcalendar/daygrid";
-// import supabaseClient from "../../utils/supabaseClient";
-// import { useEffect, useState } from "react";
-// import { CalenderType } from "../../types/calenderTypes";
-
-// function CalenderComp() {
-//   const [calEvents, setCalEvents] = useState<CalenderType[]>([]);
-
-//   const handleEvents = async () => {
-//     const { data, error } = await supabaseClient.from("Meetings").select("*");
-//     if (data) {
-//       setCalEvents(data);
-//     } else {
-//       console.log(error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     handleEvents();
-//   }, []);
-
-//   const events = calEvents.map((event) => ({
-//     ...event,
-//     id: event.id.toString()
-//   }));
-
-//   return (
-//     <div className="">
-//       <FullCalendar
-//         plugins={[dayGridPlugin]}
-//         initialView="dayGridMonth"
-//         weekends={true}
-//         events={events}
-//         eventClick={() => {
-//           const modal = document.getElementById(
-//             "my_modal_1"
-//           ) as HTMLDialogElement;
-//           if (modal) {
-//             modal.showModal();
-//           }
-//         }}
-//         eventContent={renderEventContent}
-//       />
-//     </div>
-//   );
-// }
-
-// // a custom render function
-// function renderEventContent(eventInfo: CalenderType) {
-
-//   return (
-//     <>
-//       <i
-//         onClick={() => {
-//           const modal = document.getElementById(
-//             "my_modal_1"
-//           ) as HTMLDialogElement;
-//           if (modal) {
-//             modal.showModal();
-//           }
-//         }}
-//       >
-//         {eventInfo.id}
-//       </i>
-//       {/* Open the modal using document.getElementById('ID').showModal() method */}
-
-//       <dialog id="my_modal_1" className="modal">
-//         <div className="modal-box">
-//           <h3 className="font-bold text-lg">Hello!</h3>
-//           <p className="py-4">
-//             Press ESC key or click the button below to close
-//           </p>
-//           <b>{eventInfo.org_id}</b>
-//           <i>{eventInfo.user_id}</i>
-//           <i>{eventInfo.meet_link}</i>
-//           <div className="modal-action">
-//             <form method="dialog">
-//               {/* if there is a button in form, it will close the modal */}
-
-//               <button className="btn">Close</button>
-//             </form>
-//           </div>
-//         </div>
-//       </dialog>
-//     </>
-//   );
-// }
-
-// export default CalenderComp;
-
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import supabaseClient from "../../utils/supabaseClient";
@@ -98,6 +7,7 @@ import { CalenderType } from "../../types/calenderTypes";
 function CalenderComp() {
   const [calEvents, setCalEvents] = useState<CalenderType[]>([]);
   const [selectedEvent, setSelectedEvent] = useState<CalenderType | null>(null);
+  const [userId, setUserId] = useState("");
 
   const handleEvents = async () => {
     const { data, error } = await supabaseClient.from("Meetings").select("*");
@@ -141,7 +51,7 @@ function CalenderComp() {
         weekends={true}
         events={events}
         eventClick={handleEventClick}
-        eventContent={renderEventContent}
+        eventContent={renderEventContent(userId)}
       />
 
       {selectedEvent &&
@@ -170,6 +80,7 @@ function CalenderComp() {
                 <button
                   className="btn"
                   onClick={() => {
+                    setUserId(selectedEvent.user_id);
                     const modal = document.getElementById(
                       "my_modal_1"
                     ) as HTMLDialogElement;
@@ -189,11 +100,13 @@ function CalenderComp() {
 }
 
 // Render function for calendar events
-function renderEventContent(eventInfo: CalenderType) {
+function renderEventContent(userId: string) {
   return (
-    <i className="text-xl text-white">
-      {eventInfo.id}
-    </i>
+    <div className="text-wrap overflow-hidden">
+      <p className="p-2 text-white">
+        {userId}
+      </p>
+    </div>
   );
 }
 
